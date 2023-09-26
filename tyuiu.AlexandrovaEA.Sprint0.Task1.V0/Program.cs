d936156d8fabcceed4298c603e97d1ed11937b4a
tyuiu.alexandrovaea@outlook.com <tyuiu.alexandrovaea@outlook.com>
2023-09-26 18:06:13 +0500

    Task1

---
+++ tyuiu.AlexandrovaEA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.AlexandrovaEA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string name = "Катерина";
+            int age = 21;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " год");
+            Console.ReadKey();
+
+        }
+    }
+}
